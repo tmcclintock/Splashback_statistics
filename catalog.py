5653c2c20f15586f4bc60e3af0af4f963ce20813
Tom McClintock <tmcclintock89@gmail.com>
2019-11-06 08:16:32 +0000

    Reran a notebook for slides for a talk.

--- catalog.py
+++ catalog.py
@@ -330,6 +330,9 @@ class Catalog(object):
 if __name__ == "__main__":
 #    cat = Catalog([2000,1000,500,250,125,63], 0.88534)
     cat = Catalog([2000], 0.88534)
+    df = cat.dataframe
+    df.to_pickle('./sparta_cats/L2000_N1024_CBol/df_L2000_sf1_CBol.pkl')
+    exit()
     names, corrs = cat.compute_correlations("R", "mean")
     names_partial_acc, corrs_partial_acc = cat.compute_partial_correlations("R","Acc_Rate_2*Tdyn","mean")
     names_partial_nu, corrs_partial_nu = cat.compute_partial_correlations("R","nusp_mean","mean")
